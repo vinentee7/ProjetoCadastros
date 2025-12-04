5ba2b153d60e3babec802b7ef85dd91b0c88b452
vinentee7 <samuelvinente07@gmail.com>
2025-12-04 17:15:43 -0300

    fit: Request Api, "Axios"

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,51 @@
+'use client';
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import axios from 'axios';
+
+type CharactersProps = {
+  id: string;
+  nome: string;
+  cpf: string;
+  telefone: string;
+  endereco: string;
+};
 
 export default function Home() {
+
+  const [data,setData] = useState<CharactersProps[]>([]);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    const handleFetch = async () => {
+      try{
+        const response = await axios.get('http://localhost:8000', 
+        {
+          signal: controller.signal
+        }
+      );
+
+      setData(response.data.results);
+      } catch (error){
+        console.log(error);
+      }
+    };
+
+    handleFetch();
+
+    return () => {
+      controller.abort();
+    }
+  }, []);
+
   return (
     <div>
       <main>
         <header>
           <h1>Cadastros</h1>
         </header>
-
-        <table>
-          <tr>
-            <td>ID</td>
-            <td>Nome</td>
-            <td>CPF</td>
-            <td>Telefone</td>
-            <td>Endereço</td>
-          </tr>
-        </table>
       </main>
     </div>
   );
